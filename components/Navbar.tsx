5c53ea65121eb3193962c0c52cf600fe7682e228
cliff <clifflee1022@gmail.com>
2023-08-27 11:12:23 +0800

    feat: navbar profileMenu ok

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,12 @@ import Link from "next/link"
 import AuthProviders from "./AuthProviders"
 import { NavLinks } from "@/constants"
 import { getCurrentUser } from "@/lib/session"
+import { signOut } from "next-auth/react"
+import ProfileMenu from "./ProfileMenu"
 
 const Navbar = async () => {
   const session = await getCurrentUser()
-  console.log('---session', session);
+  console.log("---session", session)
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -27,15 +29,18 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {session?.user?.image && (
-              <Image
-                src={session.user.image}
-                width={40}
-                height={40}
-                className="rounded-full"
-                alt={session.user.name}
-              />
-            )}
+          <ProfileMenu session={session}/>
+            {/* {session?.user?.image && (
+              <Link href={`/profile/${session?.user?.id}`}>
+                <Image
+                  src={session.user.image}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  alt={session.user.name}
+                />
+              </Link>
+            )} */}
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
